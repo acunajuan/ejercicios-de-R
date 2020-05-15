035cec7883738f09acf54d1178203fe41ba19b27
acunajuan <juan.acunav@utem.cl>
2020-05-15 13:28:35 -0400

    Se agrega introduccion a R y ejercicios de funciones

---
+++ Introduccion a r.R
@@ -0,0 +1,43 @@
+#Variables
+
+a<- 6
+b<- 5
+c<- 4
+
+a+b
+
+a <= b
+c <= b && b <= a
+
+a %% b
+
+#Cuando un numero es par
+#yo
+
+numeroparoimpar<- 6
+resultado <- if (a %% 2 == 0) {"El número es par"} else {
+  
+  "No es par"
+}
+
+#profe
+a <- 3
+
+a %% 2 == 0
+
+#asignar nombres
+
+a<- "hola mundo"
+a
+
+a<- TRUE
+
+!a
+
+a<- 0.4
+
+a + b
+
+
+
+
